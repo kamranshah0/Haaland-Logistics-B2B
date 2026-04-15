a5716b0c6771e079f416fce53e5730e6b67c78cf
dev-jamshed <jamshedgopang283@gmail.com>
2026-04-15 23:20:36 +0500

    design: upgrade website loader and apply branded logo assets

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,6 +1,2 @@
-<div {{ $attributes->merge(['class' => 'flex items-center gap-2']) }}>
-    <div class="w-8 h-8 bg-brand-600 rounded flex items-center justify-center font-bold text-white shadow-lg shadow-brand-600/20">
-        H
-    </div>
-    <span class="text-lg font-bold tracking-tight text-white font-outfit">HAALAND <span class="text-brand-500">LOGISTICS</span></span>
-</div>
+<img src="{{ asset('assets/Logo Red Black Large.png') }}" {{ $attributes->merge(['class' => 'h-12 w-auto object-contain']) }} alt="{{ config('app.name') }}">
+
